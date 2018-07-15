24b163662deadb25f33dc58d7eff369a9da847ae
zheenek <38116896+zheenek@users.noreply.github.com>
2018-07-16 01:00:18 +0300

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    var back = $('.closeLink'),
-        openModalWindow = $('#openM');
-    var modalWindow = $('#open');
+    var $back = $('.closeLink'),
+        $openModalWindow = $('.photo-container--button');
+    var $modalWindow = $('#open');
     
-    back.on('click', function() {
-        $(modalWindow).addClass('backTo'); 
-        $(modalWindow).removeClass('openModal');
+    $back.on('click', function() {
+        $modalWindow.addClass('backTo'); 
+        $modalWindow.removeClass('openModal');
     });
     
-    openModalWindow.on('click', function() {
-        $(modalWindow).addClass('openModal');
-        $(modalWindow).removeClass('backTo');
+    $openModalWindow.on('click', function() {
+        $modalWindow.addClass('openModal');
+        $modalWindow.removeClass('backTo');
     });
 });
