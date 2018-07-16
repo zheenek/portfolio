1db4a7222d4527fccce91d75e4613b8cadbabcc3
zheenek <38116896+zheenek@users.noreply.github.com>
2018-07-16 22:07:51 +0300

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -19,17 +19,17 @@ $(document).ready(function() {
         var target = $(e.target);
         
         if(target.is('.0')) {
-            $image.css('background-image', 'url(../img/design/design.jpg)');
+            $image.css('background-image', 'url(./img/design/design.jpg)');
         } else if (target.is('.1')) {
-            $image.css('background-image', 'url(../img/design/cblf.png)');
+            $image.css('background-image', 'url(./img/design/cblf.png)');
         } else if (target.is('.2')) {
-            $image.css('background-image', 'url(../img/design/Camaro.png)');
+            $image.css('background-image', 'url(./img/design/Camaro.png)');
         } else if (target.is('.3')) {
-            $image.css('background-image', 'url(../img/design/skull1.jpg)');
+            $image.css('background-image', 'url(./img/design/skull1.jpg)');
         } else if (target.is('.4')) {
-            $image.css('background-image', 'url(../img/design/player.png)');
+            $image.css('background-image', 'url(./img/design/player.png)');
         } else if (target.is('.5')) {
-            $image.css('background-image', 'url(../img/design/hd.jpg)');
+            $image.css('background-image', 'url(./img/design/hd.jpg)');
         }
     });
 });
