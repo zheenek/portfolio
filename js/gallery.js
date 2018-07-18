ce075bda2e7857b428bdba34527d3c440cac6cf4
zheenek <38116896+zheenek@users.noreply.github.com>
2018-07-19 00:55:12 +0300

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -10,23 +10,32 @@ $(document).ready(function() {
         $body.removeClass('noScroll');
     });
     
-    $openModalWindow.on('click', function(e) {
-        var target = $(e.target);
+    function openModal() {
         $modalWindow.addClass('openModal').removeClass('backTo');
         $body.addClass('noScroll');
+    }
+    
+    $openModalWindow.on('click', function(e) {
+        var target = $(e.target);
         
         if(target.is('.0')) {
             $image.css('background-image', 'url(./img/design/design.jpg)');
+            openModal();
         } else if (target.is('.1')) {
             $image.css('background-image', 'url(./img/design/cblf.png)');
+            openModal();
         } else if (target.is('.2')) {
             $image.css('background-image', 'url(./img/design/Camaro.png)');
+            openModal();
         } else if (target.is('.3')) {
             $image.css('background-image', 'url(./img/design/skull1.jpg)');
+            openModal();
         } else if (target.is('.4')) {
             $image.css('background-image', 'url(./img/design/player.png)');
+            openModal();
         } else if (target.is('.5')) {
             $image.css('background-image', 'url(./img/design/hd.jpg)');
+            openModal();
         } else {}
     });
 });
